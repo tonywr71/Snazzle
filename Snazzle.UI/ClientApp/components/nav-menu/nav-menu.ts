35c4af197a793c7ed52c77fa3a55add294e83f40
tonywr71 <tonyz.wrightz@consultant.com>
2016-09-15 14:46:26 +1000

    hide menu item if not logged in

--- Snazzle.UI/ClientApp/components/nav-menu/nav-menu.ts
+++ Snazzle.UI/ClientApp/components/nav-menu/nav-menu.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/AuthenticationService';
 
 @Component({
   selector: 'nav-menu',
   template: require('./nav-menu.html')
 })
 export class NavMenu {
+
+    constructor(private authenticationService: AuthenticationService) {
+    }
+
+    isAuthorized(): boolean {
+        return this.authenticationService.isAuthorized();
+    }
 }
