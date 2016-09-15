48884dc62054d4952bccb77e710a54e5263cd297
tonywr71 <tonyz.wrightz@consultant.com>
2016-09-15 17:46:19 +1000

    Addition of google  login backend code (not complete)

--- Snazzle.WebApi/Snazzle.WebApi/Startup.cs
+++ Snazzle.WebApi/Snazzle.WebApi/Startup.cs
@@ -98,6 +98,12 @@ namespace Snazzle.WebApi
 
       app.UseIdentity();
 
+      app.UseGoogleAuthentication(new GoogleOptions
+      {
+        ClientId = "560027070069-37ldt4kfuohhu3m495hk2j4pjp92d382.apps.googleusercontent.com",
+        ClientSecret = "n2Q-GEw9RQjzcRbU3qhfTj8f"
+      });
+
       app.UseOAuthValidation();
 
       app.UseOpenIddict();
