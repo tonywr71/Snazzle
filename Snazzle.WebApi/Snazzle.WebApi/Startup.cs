9bd06a79bebf924015d50c6a9703bf646ca401ad
tonywr71 <tonyz.wrightz@consultant.com>
2016-09-23 19:44:36 +1000

    cleanup

--- Snazzle.WebApi/Snazzle.WebApi/Startup.cs
+++ Snazzle.WebApi/Snazzle.WebApi/Startup.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Snazzle.WebApi.Services;
 using Snazzle.WebApi.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using System.IO;
-using OpenIddict;
-using CryptoHelper;
+using Snazzle.WebApi.Services;
 
 namespace Snazzle.WebApi
 {
